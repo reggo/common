bf2298813524560b21864cdaae65eeb0a39f8138
btracey <bdtinternet@gmail.com>
2014-01-20 14:29:43 -0800

    Changed Verify to take a matrix

--- errors.go
+++ errors.go
@@ -17,14 +17,14 @@ func (d DataMismatch) Error() string {
 	return fmt.Sprintf("reggo: length mismatch. inputs: %v, outputs: %v, weights: %v ", d.Input, d.Output, d.Weight)
 }
 
-var InputLengths error = errors.New("reggo: inputs do not all have the same length")
-var OutputLengths error = errors.New("reggo: outputs do not all have the same length")
+var InputDimension error = errors.New("reggo: input dimension mismatch")
+var OutputLengths error = errors.New("reggo: output dimension mismatch")
 var NoData error = errors.New("reggo: nil data")
 
 // VerifyInputs returns true if the number of rows in inputs is not the same
 // as the number of rows in outputs and the length of weights. As a special case,
 // the length of weights is allowed to be zero.
-func VerifyInputs(inputs, outputs *mat64.Dense, weights []float64) error {
+func VerifyInputs(inputs, outputs mat64.Matrix, weights []float64) error {
 	if inputs == nil || outputs == nil {
 		return NoData
 	}
