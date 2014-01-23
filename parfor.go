2d09c2c1a317f181133ddafbf17655fc65146715
btracey <bdtinternet@gmail.com>
2014-01-23 11:16:12 -0800

    Added MutableRowMatrix and moved interfaces to their own file

--- parfor.go
+++ parfor.go
@@ -4,14 +4,7 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
-
-	"github.com/gonum/matrix/mat64"
 )
-
-type RowMatrix interface {
-	mat64.Matrix
-	Row([]float64, int) []float64
-}
 
 // GetGrainSize returns a reasonable value to use in Grain
 func GetGrainSize(nSamples, minGrainSize, maxGrainSize int) int {
